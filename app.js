0b502bb360acac774b012c5f6b53103611d02ae1
Glenn Goossens <glenngoossens07@gmail.com>
2017-11-23 17:13:57 +0100

    adding auth

--- app.js
+++ app.js
@@ -2,37 +2,60 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
 var Campground = require("./models/campground");
 var Comment = require("./models/comment");
-/*
 var User = require("./models/user");
-*/
+
 var seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
+//PASSPORT configure
+app.use(require("express-session")({
+  secret: "Rusty is the best",
+  resave:false,
+  saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
+//HOME
 app.get("/", function(req, res) {
   res.render("landing");
 
 });
 
+//campgrounds
 app.get("/campgrounds", function(req, res) {
   Campground.find({}, function(err, c) {
     if (err) {
       console.log(err);
     } else {
-      res.render("index", {campgrounds: c})
+      res.render("campgrounds/index", {campgrounds: c});
     }
   });
 });
 
+//Adding campground form
 app.get("/campgrounds/new", function(req, res) {
-  res.render("new");
+  res.render("campgrounds/new");
 });
 
+//posting campground
 app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
@@ -52,16 +75,98 @@ app.post("/campgrounds", function(req, res) {
   });
 });
 
+//details of campground
 app.get("/campgrounds/:id", function(req, res) {
   Campground.findById(req.params.id).populate("comments").exec(function(err, found) {
     if (err) {
       console.log(err);
     } else {
       console.log(found);
-      res.render("show", {campground: found});
+      res.render("campgrounds/show", {campground: found});
     }
   });
 });
+
+/*
+==================COMMENTS ROUTES=====================
+*/
+//adding comment form
+app.get("/campgrounds/:id/comments/new",isLoggedIn, function(req, res) {
+  Campground.findById(req.params.id, function(err, campground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground: campground});
+    }
+  });
+});
+
+app.post("/campgrounds/:id/comments",isLoggedIn, function(req, res) {
+  //loop up camground id
+  Campground.findById(req.params.id, function(err, campground) {
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      //create new comment
+      Comment.create(req.body.comment, function(err, comment) {
+        if (err) {
+          console.log(err);
+        } else {
+          //connect new comment to campgrounds
+          campground.comments.push(comment);
+          campground.save();
+          //redirect to show page of campground
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      });
+    }
+  });
+
+});
+
+//AUTH
+//show register form
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+//post register form
+app.post("/register",function(req,res){
+  User.register(new User({username: req.body.username}),req.body.password,function(err,user){
+    if(err){
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/campgrounds");
+    });
+  });
+});
+
+//Show login form
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.post("/login",passport.authenticate("local",{
+  successRedirect:"/campgrounds",
+  failureRedirect:"/login"
+}),function(req,res){
+});
+
+//logout
+app.get("/logout",function(req,res){
+  res.logout();
+  res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(3000, function(req, res) {
   console.log("server is started and running on port 3000");
--- seeds.js
+++ seeds.js
@@ -6,39 +6,48 @@ var data = [
   {
     name: "Salmon Creek",
     image: "http://www.photosforclass.com/download/2617191414",
-    description: "This is Salmon Creek"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel ex interdum, rhoncus enim eget, dictum nibh. Etiam tincidunt dolor sed ultrices facilisis. Etiam pulvinar, sapien vel pellentesque auctor, turpis lorem pharetra nisl, eget mollis risus nisl at sem. Nulla laoreet elit sapien, nec maximus dolor laoreet malesuada. Donec ut nunc orci. Maecenas ac libero commodo, pretium magna nec, congue lectus. In eleifend mi libero, eget ornare enim condimentum vel. Pellentesque in nulla felis. Etiam sodales pulvinar diam, quis cursus erat rutrum eu. Nullam arcu lorem, pharetra nec ipsum quis, eleifend porttitor metus. Phasellus ut risus aliquet, tincidunt risus a, mattis tellus. Pellentesque urna urna, dictum et volutpat sollicitudin, ornare a turpis."
+
   }, {
     name: "Granite Hill",
     image: "http://www.photosforclass.com/download/2602356334",
-    description: "This is Granite Hill"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel ex interdum, rhoncus enim eget, dictum nibh. Etiam tincidunt dolor sed ultrices facilisis. Etiam pulvinar, sapien vel pellentesque auctor, turpis lorem pharetra nisl, eget mollis risus nisl at sem. Nulla laoreet elit sapien, nec maximus dolor laoreet malesuada. Donec ut nunc orci. Maecenas ac libero commodo, pretium magna nec, congue lectus. In eleifend mi libero, eget ornare enim condimentum vel. Pellentesque in nulla felis. Etiam sodales pulvinar diam, quis cursus erat rutrum eu. Nullam arcu lorem, pharetra nec ipsum quis, eleifend porttitor metus. Phasellus ut risus aliquet, tincidunt risus a, mattis tellus. Pellentesque urna urna, dictum et volutpat sollicitudin, ornare a turpis."
+
   }, {
     name: "Mountain Goat",
     image: "http://www.photosforclass.com/download/6015893151",
-    description: "This is Mountain Goat"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel ex interdum, rhoncus enim eget, dictum nibh. Etiam tincidunt dolor sed ultrices facilisis. Etiam pulvinar, sapien vel pellentesque auctor, turpis lorem pharetra nisl, eget mollis risus nisl at sem. Nulla laoreet elit sapien, nec maximus dolor laoreet malesuada. Donec ut nunc orci. Maecenas ac libero commodo, pretium magna nec, congue lectus. In eleifend mi libero, eget ornare enim condimentum vel. Pellentesque in nulla felis. Etiam sodales pulvinar diam, quis cursus erat rutrum eu. Nullam arcu lorem, pharetra nec ipsum quis, eleifend porttitor metus. Phasellus ut risus aliquet, tincidunt risus a, mattis tellus. Pellentesque urna urna, dictum et volutpat sollicitudin, ornare a turpis."
+
   }, {
     name: "Glenn Creek",
     image: "http://www.photosforclass.com/download/4369518024",
-    description: "This is Glenn Creek"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel ex interdum, rhoncus enim eget, dictum nibh. Etiam tincidunt dolor sed ultrices facilisis. Etiam pulvinar, sapien vel pellentesque auctor, turpis lorem pharetra nisl, eget mollis risus nisl at sem. Nulla laoreet elit sapien, nec maximus dolor laoreet malesuada. Donec ut nunc orci. Maecenas ac libero commodo, pretium magna nec, congue lectus. In eleifend mi libero, eget ornare enim condimentum vel. Pellentesque in nulla felis. Etiam sodales pulvinar diam, quis cursus erat rutrum eu. Nullam arcu lorem, pharetra nec ipsum quis, eleifend porttitor metus. Phasellus ut risus aliquet, tincidunt risus a, mattis tellus. Pellentesque urna urna, dictum et volutpat sollicitudin, ornare a turpis."
+
   }, {
     name: "Yana Hill",
     image: "http://www.photosforclass.com/download/4684194306",
-    description: "This is Yana Hill"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel ex interdum, rhoncus enim eget, dictum nibh. Etiam tincidunt dolor sed ultrices facilisis. Etiam pulvinar, sapien vel pellentesque auctor, turpis lorem pharetra nisl, eget mollis risus nisl at sem. Nulla laoreet elit sapien, nec maximus dolor laoreet malesuada. Donec ut nunc orci. Maecenas ac libero commodo, pretium magna nec, congue lectus. In eleifend mi libero, eget ornare enim condimentum vel. Pellentesque in nulla felis. Etiam sodales pulvinar diam, quis cursus erat rutrum eu. Nullam arcu lorem, pharetra nec ipsum quis, eleifend porttitor metus. Phasellus ut risus aliquet, tincidunt risus a, mattis tellus. Pellentesque urna urna, dictum et volutpat sollicitudin, ornare a turpis."
+
   }, {
     name: "Yanni Goat",
     image: "http://www.photosforclass.com/download/8524305204",
-    description: "This is Yanni Goat"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel ex interdum, rhoncus enim eget, dictum nibh. Etiam tincidunt dolor sed ultrices facilisis. Etiam pulvinar, sapien vel pellentesque auctor, turpis lorem pharetra nisl, eget mollis risus nisl at sem. Nulla laoreet elit sapien, nec maximus dolor laoreet malesuada. Donec ut nunc orci. Maecenas ac libero commodo, pretium magna nec, congue lectus. In eleifend mi libero, eget ornare enim condimentum vel. Pellentesque in nulla felis. Etiam sodales pulvinar diam, quis cursus erat rutrum eu. Nullam arcu lorem, pharetra nec ipsum quis, eleifend porttitor metus. Phasellus ut risus aliquet, tincidunt risus a, mattis tellus. Pellentesque urna urna, dictum et volutpat sollicitudin, ornare a turpis."
+
   }, {
     name: "Jens Huts",
     image: "http://www.photosforclass.com/download/6037590541",
-    description: "This is Jens Huts"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel ex interdum, rhoncus enim eget, dictum nibh. Etiam tincidunt dolor sed ultrices facilisis. Etiam pulvinar, sapien vel pellentesque auctor, turpis lorem pharetra nisl, eget mollis risus nisl at sem. Nulla laoreet elit sapien, nec maximus dolor laoreet malesuada. Donec ut nunc orci. Maecenas ac libero commodo, pretium magna nec, congue lectus. In eleifend mi libero, eget ornare enim condimentum vel. Pellentesque in nulla felis. Etiam sodales pulvinar diam, quis cursus erat rutrum eu. Nullam arcu lorem, pharetra nec ipsum quis, eleifend porttitor metus. Phasellus ut risus aliquet, tincidunt risus a, mattis tellus. Pellentesque urna urna, dictum et volutpat sollicitudin, ornare a turpis."
+
   }, {
     name: "Yosemite",
     image: "http://www.photosforclass.com/download/7121858075",
-    description: "This is Yosemite"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel ex interdum, rhoncus enim eget, dictum nibh. Etiam tincidunt dolor sed ultrices facilisis. Etiam pulvinar, sapien vel pellentesque auctor, turpis lorem pharetra nisl, eget mollis risus nisl at sem. Nulla laoreet elit sapien, nec maximus dolor laoreet malesuada. Donec ut nunc orci. Maecenas ac libero commodo, pretium magna nec, congue lectus. In eleifend mi libero, eget ornare enim condimentum vel. Pellentesque in nulla felis. Etiam sodales pulvinar diam, quis cursus erat rutrum eu. Nullam arcu lorem, pharetra nec ipsum quis, eleifend porttitor metus. Phasellus ut risus aliquet, tincidunt risus a, mattis tellus. Pellentesque urna urna, dictum et volutpat sollicitudin, ornare a turpis."
+
   }, {
     name: "Sierra",
     image: "http://www.photosforclass.com/download/37433523451",
-    description: "This is Sierra"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel ex interdum, rhoncus enim eget, dictum nibh. Etiam tincidunt dolor sed ultrices facilisis. Etiam pulvinar, sapien vel pellentesque auctor, turpis lorem pharetra nisl, eget mollis risus nisl at sem. Nulla laoreet elit sapien, nec maximus dolor laoreet malesuada. Donec ut nunc orci. Maecenas ac libero commodo, pretium magna nec, congue lectus. In eleifend mi libero, eget ornare enim condimentum vel. Pellentesque in nulla felis. Etiam sodales pulvinar diam, quis cursus erat rutrum eu. Nullam arcu lorem, pharetra nec ipsum quis, eleifend porttitor metus. Phasellus ut risus aliquet, tincidunt risus a, mattis tellus. Pellentesque urna urna, dictum et volutpat sollicitudin, ornare a turpis."
+
   }
 ]
 
